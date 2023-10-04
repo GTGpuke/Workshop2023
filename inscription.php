27396fc38c4fb699447e4dd47a6d2ede0dfbfbe9
Pierre Ledeuil <pierre.ledeuil@epsi.fr>
2023-10-04 09:51:08 +0200

    Changement dans le login, et modifications archi

--- inscription.php
+++ inscription.php
@@ -7,22 +7,29 @@
 
             <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-            <title>Connexion</title>
+            <title>Inscription</title>
         </head>
         <body>
-        
         <div class="login-form">
-             <?php 
-                if(isset($_GET['login_err']))
+            <?php 
+                if(isset($_GET['reg_err']))
                 {
-                    $err = htmlspecialchars($_GET['login_err']);
+                    $err = htmlspecialchars($_GET['reg_err']);
 
                     switch($err)
                     {
+                        case 'success':
+                        ?>
+                            <div class="alert alert-success">
+                                <strong>Succès</strong> inscription réussie !
+                            </div>
+                        <?php
+                        break;
+
                         case 'password':
                         ?>
                             <div class="alert alert-danger">
-                                <strong>Erreur</strong> mot de passe incorrect
+                                <strong>Erreur</strong> mot de passe différent
                             </div>
                         <?php
                         break;
@@ -30,24 +37,41 @@
                         case 'email':
                         ?>
                             <div class="alert alert-danger">
-                                <strong>Erreur</strong> email incorrect
+                                <strong>Erreur</strong> email non valide
                             </div>
                         <?php
                         break;
 
+                        case 'email_length':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email trop long
+                            </div>
+                        <?php 
+                        break;
+
+                        case 'pseudo_length':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> pseudo trop long
+                            </div>
+                        <?php 
                         case 'already':
                         ?>
                             <div class="alert alert-danger">
-                                <strong>Erreur</strong> compte non existant
+                                <strong>Erreur</strong> compte deja existant
                             </div>
-                        <?php
-                        break;
+                        <?php 
+
                     }
                 }
-                ?> 
+                ?>
             
-            <form action="connexion.php" method="post">
-                <h2 class="text-center">Connexion</h2>       
+            <form action="inscription_traitement.php" method="post">
+                <h2 class="text-center">Inscription</h2>       
+                <div class="form-group">
+                    <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
+                </div>
                 <div class="form-group">
                     <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
                 </div>
@@ -55,10 +79,12 @@
                     <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+                    <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">Inscription</button>
                 </div>   
             </form>
-            <p class="text-center"><a href="inscription.php">Inscription</a></p>
         </div>
         <style>
             .login-form {
