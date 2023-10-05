4bfe057a56a493cc02da5b9c5544345271e2ba9d
Pierre Ledeuil <pierre.ledeuil@epsi.fr>
2023-10-05 22:27:31 +0200

    account system

--- account.php
+++ account.php
@@ -100,11 +100,98 @@
       <!-- header section end -->
       <!-- services section start -->
       <div class="services_section layout_padding">
-         <div class="container">
-            <div class="play_icon_main" style="-moz-border-radius: 5%; -webkit-border-radius: 5%;border-radius: 10%;overflow:hidden;position:relative;">
-            <iframe style=" height: 500px; width: 100%; border: none;" class="aaaaaaaaaaaaaaaaaaaaaa" src="https://montpellier3m.fr/collecte" frameborder="0" allowfullscreen></iframe>
+        <div class="container">
+        <div class="container">
+            <div class="col-md-12">
+                <?php 
+                        if(isset($_GET['err'])){
+                            $err = htmlspecialchars($_GET['err']);
+                            switch($err){
+                                case 'current_password':
+                                    echo "<div class='alert alert-danger'>Le mot de passe actuel est incorrect</div>";
+                                break;
+
+                                case 'success_password':
+                                    echo "<div class='alert alert-success'>Le mot de passe a bien été modifié ! </div>";
+                                break; 
+                            }
+                        }
+                    ?>
+
+
+                <div class="text-center">
+                        <h1 class="p-5">Bonjour <?php echo $data['pseudo']; ?> !</h1>
+                        <hr />
+                        <a href="deconnexion.php" class="btn btn-danger btn-lg">Déconnexion</a>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#change_password">
+                          Changer mon mot de passe
+                        </button>
+                </div>
             </div>
-         </div>
+        </div>    
+
+       
+
+        
+
+                                
+        <!-- Modal -->
+        <div class="modal fade" id="change_password" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Changer mon mot de passe</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                         </div>
+                            <div class="modal-body">
+                                <form action="layouts/change_password.php" method="POST">
+                                    <label for='current_password'>Mot de passe actuel</label>
+                                    <input type="password" id="current_password" name="current_password" class="form-control" required/>
+                                    <br />
+                                    <label for='new_password'>Nouveau mot de passe</label>
+                                    <input type="password" id="new_password" name="new_password" class="form-control" required/>
+                                    <br />
+                                    <label for='new_password_retype'>Re tapez le nouveau mot de passe</label>
+                                    <input type="password" id="new_password_retype" name="new_password_retype" class="form-control" required/>
+                                    <br />
+                                    <button type="submit" class="btn btn-success">Sauvegarder</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="modal fade" id="avatar" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Changer mon avatar</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="layouts/change_avatar.php" method="POST" enctype="multipart/form-data">
+                                <label for="avatar">Images autorisées : png, jpg, jpeg, gif - max 20Mo</label>
+                                <input type="file" name="avatar_file">
+                                <br />
+                                <button type="submit" class="btn btn-success">Modifier</button>
+                            </form>
+                        </div>
+                        <br />
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
       <!-- services section end -->
       <!-- footer section start -->
